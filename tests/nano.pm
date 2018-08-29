ce8282573143ad29b9c94dba05292ab840c35194
antonio <a.caristia@gmail.com>
2018-08-29 16:46:11 +0200

    added needles and sources

---
+++ tests/nano.pm
@@ -0,0 +1,46 @@
+# Copyright (C) 2014-2017 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+use base 'basetest';
+use strict;
+use testapi;
+
+sub run {
+	# get a tty
+	send_key "ctrl-alt-f1";
+	assert_screen "console", 30;
+
+	# login as root
+	type_string "root";
+	send_key "ret";
+	type_password "root";
+	send_key "ret";
+
+	# start nano
+	type_string "nano";
+	send_key "ret";
+	assert_screen "nano";
+}
+
+sub test_flags {
+    # 'fatal'          - abort whole test suite if this fails (and set overall state 'failed')
+    # 'ignore_failure' - if this module fails, it will not affect the overall result at all
+    # 'milestone'      - after this test succeeds, update 'lastgood'
+    # 'norollback'     - don't roll back to 'lastgood' snapshot if this fails
+    return { fatal => 1 };
+}
+
+1;
+
